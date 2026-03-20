ff48e6ff047df9ccd019702737785e3f32c370cf
Meek Meek <haggins.jameka@gmail.com>
2026-03-20 15:53:48 -0400

    updated my bookRoutes to have the function of delete and update. I tested delete, that works like a charm, but update I will save for another day. My eyes hurt and I keep getting errors because I'm spamming typos like crazy.

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -35,5 +35,35 @@ router.post('/', async (req, res) => {
 
 });
 
+router.delete('/:id', async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const deletedBook = await Book.findByIdAndDelete(bookId)
+
+    if (!deletedBook) {
+      return res.status(404).json({ message: "Cannot delete: Book not found." });
+    }
+    res.json({ message: "Book successfully removed from the shelf!", deletedBook })
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting book", error: error.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const updateData = req.body;
+
+    const updatedBook = await Book.findByIdAndUpdate(bookId, updateData, { new: true });
+
+    if (!updatedBook) {
+      return res.status(404).json({ message: "Book not found to update" });
+    }
+    res.json(updatedBook);
+  } catch (error) {
+    res.status(500).json({ message: "Error updating book", error: error.message });
+  }
+});
+
 
 export default router;
